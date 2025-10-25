ec625e14101066da79e244cdba853f15f083651e
Ziya <Ziyasgar@gmail.com>
2025-10-25 16:57:34 -0700

    reformatted the index.sql file

--- index.sql
+++ index.sql
@@ -1,45 +1,42 @@
 -- Create tables for customers, products, and sales
-CREATE TABLE
-  dim_customers (
-    customer_key int,
-    customer_id int,
-    customer_number varchar(50),
-    first_name varchar(50),
-    last_name varchar(50),
-    country varchar(50),
-    marital_status varchar(50),
-    gender varchar(50),
-    birthdate date,
-    create_date date
-  );
+CREATE TABLE dim_customers (
+  customer_key int,
+  customer_id int,
+  customer_number varchar(50),
+  first_name varchar(50),
+  last_name varchar(50),
+  country varchar(50),
+  marital_status varchar(50),
+  gender varchar(50),
+  birthdate date,
+  create_date date
+);
 
-CREATE TABLE
-  dim_products (
-    product_key int,
-    product_id int,
-    product_number varchar(50),
-    product_name varchar(50),
-    category_id varchar(50),
-    category varchar(50),
-    subcategory varchar(50),
-    maintenance varchar(50),
-    cost int,
-    product_line varchar(50),
-    start_date date
-  );
+CREATE TABLE dim_products (
+  product_key int,
+  product_id int,
+  product_number varchar(50),
+  product_name varchar(50),
+  category_id varchar(50),
+  category varchar(50),
+  subcategory varchar(50),
+  maintenance varchar(50),
+  cost int,
+  product_line varchar(50),
+  start_date date
+);
 
-CREATE TABLE
-  fact_sales (
-    order_number varchar(50),
-    product_key int,
-    customer_key int,
-    order_date date,
-    shipping_date date,
-    due_date date,
-    sales_amount int,
-    quantity int,
-    price int
-  );
+CREATE TABLE fact_sales (
+  order_number varchar(50),
+  product_key int,
+  customer_key int,
+  order_date date,
+  shipping_date date,
+  due_date date,
+  sales_amount int,
+  quantity int,
+  price int
+);
 
 -- Copy the data from csv files to the tables
 COPY dim_customers (
